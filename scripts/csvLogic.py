7dc8c87bc42848c6aab89b628c60117b1fe61118
Jurjen de Jonge <jdejonge@protonmail.ch>
2019-10-04 08:27:11 +0300

    Updated

--- scripts/csvLogic.py
+++ scripts/csvLogic.py
@@ -20,6 +20,8 @@ def filechanged(changed_files, all_hashes):
     print(files)
 
 def checkPrevious(device, hashes):
+    if(x == "")
+        return 0
 
     r = requests.get(device)
     x = json.loads(json.dumps(r.json()))
